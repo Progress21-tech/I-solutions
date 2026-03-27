31690e40f129194d09d7bfd3929aba2e71d8e3bb
Progress Oni <dreaxellprobe@gmail.com>
2026-03-27 23:43:44 +0100

    Update next.config.js

--- mediconnect-uphr/next.config.js
+++ mediconnect-uphr/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY,
+  },
+  webpack: (config) => {
+    config.cache = false
+    return config
+  }
+}
 
 module.exports = nextConfig

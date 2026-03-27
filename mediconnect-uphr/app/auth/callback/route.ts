5be726a022d27f23c51180e6e419e9e73b255232
Progress Oni <dreaxellprobe@gmail.com>
2026-03-27 16:02:10 +0100

    Update route.ts

--- mediconnect-uphr/app/auth/callback/route.ts
+++ mediconnect-uphr/app/auth/callback/route.ts
@@ -2,6 +2,5 @@ import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url)
-  // Pass everything to client side to handle
-  return NextResponse.redirect(`${requestUrl.origin}/auth/confirm${requestUrl.search}`)
+  return NextResponse.redirect(`${requestUrl.origin}/auth/confirm?${requestUrl.searchParams}`)
 }
